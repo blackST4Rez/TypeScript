739b961fd2d42fb521893611fd55105b2a14fa48
blackST4Rez <maharjanraka2015@gmail.com>
2026-09-07 15:22:18 +0545

    Update: Interface was started

--- Basics/main.ts
+++ Basics/main.ts
@@ -5,10 +5,10 @@
 // console.log(greet(42));
 
 //Declaring type annotation
-let user: string = 'Raka'
-let age: number = 25
-let isActive: boolean = true
-console.log(`${user} is ${age} years old.`);
+// let user: string = 'Raka'
+// let age: number = 25
+// let isActive: boolean = true
+// console.log(`${user} is ${age} years old.`);
 
 {/*Type Assignment Error */}
 // let city: string = 'Kathmandu'
@@ -44,5 +44,58 @@ console.log(`${user} is ${age} years old.`);
 // console.log(isLoggedIn(24));
 // console.log(isLoggedIn('ungabunga'));
 
+//Inference in Array
+
+// const hero = ['shaktiman', 'tota', 'perman'];
+// const number = [1, 2, 3];
+// const mixed = ['crazy', 23, 'hola']
+
+// //String only accepts string data, numbers accepts number but does not accept anything data other than its types
+// hero.push('pencilman') //Only a string can be pushed in an array of string cause of types
+// number.push(34) //Same as above 
+// mixed.push('crazyman', 88) //Same as above
+
+// console.log(hero);
+// console.log(number);
+// console.log(mixed);
+
+
+//Inference in Objects
+
+// const user = {
+//     name: 'Raka',
+//     age: 23,
+//     isAdmin: false,
+//     city: {
+//         name: "New York",
+//         population: 8000000,
+//         country: "USA",
+//         coordinates: {
+//             lat: 40.7128,
+//             lng: -74.0060
+//         }
+//     }
+// }
+
+// console.log(user);
+
+//Here the num gets the type context from the type of number
+// const numbers = [1, 2, 4, 5 ,6 ,8]
+
+// numbers.forEach((num) => {
+//     console.log(`The number is ${num}.`);
+// })
+
+// interface User{
+//     name: string;
+//     age: number;
+//     isAvailable?: boolean;
+// }
+
+// let user1: User = { name: 'unga', age: 99, isAvailable:true };
+// let user2: User = { name: 'bunga', age: 1, isAvailable:false };
+
+// console.log(`${user1.name} is ${user1.age} years old. He is ${user1.isAvailable}`);
+// console.log(`${user2.name} is ${user2.age} years old. He is ${user2.isAvailable}`);
 
 
